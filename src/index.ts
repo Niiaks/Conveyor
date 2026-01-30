f1f30a9a72d34f280d76ce7b96ae6a300e973e71
Niiaks <napappoe007@st.ug.edu.gh>
2026-01-30 15:02:41 +0000

    feat: initialize database connection on server start

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
+import "dotenv/config";
 import express from "express";
+import { connectDB } from "./database";
 
-const app = express();
 const port = process.env.PORT || 3000;
+const app = express();
 
 app.listen(port, () => {
+  connectDB();
   console.log(`server is running on port ${port}`);
 });
