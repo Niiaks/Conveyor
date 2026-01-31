c2dbb4968d2bb6be077b2883d1ec7a38cd6c09ba
Niiaks <napappoe007@st.ug.edu.gh>
2026-01-31 21:09:34 +0000

    feat: set up express middleware for JSON parsing, URL encoding, and CORS

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
 import { connectDB } from "./database";
-
+import { router } from "./router";
 const port = process.env.PORT || 3000;
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.use("/api/v1", router);
 app.listen(port, () => {
   connectDB();
   console.log(`server is running on port ${port}`);
